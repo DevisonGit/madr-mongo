438ac8442b8b767667cf89b30d1e31875d5cdfc7
devisono <devison_santos@yahoo.com.br>
2025-06-12 20:54:58 -0300

    refatorando users para sanitizar o name

--- src/madr/users/repository.py
+++ src/madr/users/repository.py
@@ -2,9 +2,7 @@ from pydantic import EmailStr
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.madr.security import get_password_hash
 from src.madr.users.models import User
-from src.madr.users.schemas import UserCreate, UserUpdate
 
 
 async def get_user(username: str, email: EmailStr, session: AsyncSession):
@@ -13,23 +11,14 @@ async def get_user(username: str, email: EmailStr, session: AsyncSession):
     )
 
 
-async def create_user(user_data: UserCreate, session: AsyncSession):
-    data = user_data.model_dump()
-    data['password'] = get_password_hash(data['password'])
-    db_user = User(**data)
-    session.add(db_user)
+async def create_user(user_data: User, session: AsyncSession):
+    session.add(user_data)
     await session.commit()
-    await session.refresh(db_user)
-    return db_user
+    await session.refresh(user_data)
+    return user_data
 
 
-async def update_user(
-    user_data: UserUpdate, user_db: User, session: AsyncSession
-):
-    data = user_data.model_dump()
-    data['password'] = get_password_hash(data['password'])
-    for key, value in data.items():
-        setattr(user_db, key, value)
+async def update_user(user_db: User, session: AsyncSession):
     await session.commit()
     await session.refresh(user_db)
     return user_db
